66007d8b3b9d7a04461f837a25bd31d5c1b15945
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-01-29 00:14:59 +0200

    1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
+
+const galleryDivEl = document.querySelector(`.gallery`);
+
+galleryDivEl.innerHTML = galleryItems.map(({ preview, original, description }) => {
+        return `
+          <li>
+            <a class="gallery__item" href="${original}">
+                 <img class="gallery__image" src=${preview} alt=${description}/>
+            </a>
+          </li>
+        `
+            ;
+}).join(``);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
